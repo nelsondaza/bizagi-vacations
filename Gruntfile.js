070af61cfac8e8fd2c6a6e4791f28363f00a2072
Nelson Daza <nelson.daza@gmail.com>
2016-08-19 01:12:44 -0500

    Added styles core system

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,74 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-		// nodemon let me start the server with grunt, and reload it with avery change.
+		// Check JS files for errors
+		jshint: {
+			all: ['public/src/js/**/*.js', 'public/src/js/*.js']
+		},
+
+		// Minify JS files into app.min.js
+		uglify: {
+			build: {
+				files: {
+					'public/js/app.min.js': ['public/src/js/**/*.js', 'public/src/js/*.js']
+				}
+			}
+		},
+
+		// Process the less files to style.css
+		less: {
+			build: {
+				files: {
+					'public/css/style.css': ['public/src/css/00-main.less']
+				}
+			}
+		},
+
+		// Minify CSS styles.css into style.min.css
+		cssmin: {
+			build: {
+				files: {
+					'public/css/style.min.css': 'public/css/style.css'
+				}
+			}
+		},
+
+		// Watch files for changes process them
+		watch: {
+			css: {
+				files: ['public/src/css/00-main.less'],
+				tasks: ['less', 'cssmin']
+			},
+			js: {
+				files: ['public/src/js/**/*.js', 'public/src/js/*.js'],
+				tasks: ['jshint', 'uglify']
+			}
+		},
+
+		// nodemon let me start the server with grunt, and reload it with every change.
 		nodemon: {
 			dev: {
 				script: 'server.js'
 			}
+		},
+
+		// run watch and nodemon at the same time
+		concurrent: {
+			options: {
+				logConcurrentOutput: true
+			},
+			tasks: ['nodemon', 'watch']
 		}
+
 	});
 
-	// load nodemon
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-nodemon');
+	grunt.loadNpmTasks('grunt-concurrent');
 
-	// register the nodemon task when we run grunt
-	grunt.registerTask('default', ['nodemon']);
+	grunt.registerTask('default', ['less', 'cssmin', 'jshint', 'uglify', 'concurrent']);
 
 };
