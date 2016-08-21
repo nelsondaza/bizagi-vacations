0246a29f2c9111d3603cf9fd7bccb0a29a5b8687
Nelson Daza <nelson.daza@gmail.com>
2016-08-21 01:49:42 -0500

    General graphic interface

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,1 +1,62 @@
-//"use strict";
+$(function(){
+
+	$.mockjax({
+		url: "/api/processes",
+		proxy: "/api/inbox.json",
+		responseTime: [500, 750]
+	});
+
+	$.mockjax({
+		url: "/api/cases",
+		proxy: "/api/cases.json",
+		responseTime: [500, 750]
+	});
+
+
+	$(window).on('hashchange', hashchange);
+
+	function hashchange( ){
+		console.debug(window.location.hash);
+		switch(window.location.hash) {
+			case '#all':
+				all();
+				break;
+			case '#approved':
+				approved();
+				break;
+			case '#denied':
+				denied();
+				break;
+			default:
+				pending();
+		}
+	}
+
+	function selectMenu( hash ) {
+		$('.menu li.active').removeClass('active');
+		$('.menu li a[href="#' + hash + '"]').parent().addClass('active');
+	}
+
+	function pending() {
+		console.debug('pending');
+		selectMenu('pending');
+	}
+
+	function all() {
+		console.debug('all');
+		selectMenu('all');
+	}
+
+	function approved() {
+		console.debug('approved');
+		selectMenu('approved');
+	}
+
+	function denied() {
+		console.debug('denied');
+		selectMenu('denied');
+	}
+
+	hashchange();
+
+});
